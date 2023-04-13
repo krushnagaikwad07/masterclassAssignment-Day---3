891526ca99aa600854cfb81b39cc34c2fce15a54
krushnagaikwad07 <krushnagaikwad7799@gmail.com>
2023-04-13 12:30:41 +0530

    Assignment 12th april

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+// import './App.css';
 
+const options = [
+  {
+    label: "Apple",
+    Value: "apple",
+  },
+  {
+    label: "Mango",
+    Value: "mango",
+  },
+  {
+    label: "Banana",
+    Value: "banana",
+  },
+  {
+    label: "Pineapple",
+    Value: "pineapple",
+  },
+]
 function App() {
+
+  const [inputList, setInputList] = useState([{selectItem:'', selectQuantity:''}]);
+  const handleaddclick = () => { 
+    let users = {
+      id: Date.now(),
+      
+
+    }
+    setInputList([...inputList,{selectItem:'', selectQuantity:''}])
+  }
+
+  const handleinputchange=(e, index) =>{
+    const {item,  quantity}= e.target;
+    const list=[...inputList];
+    list[index][item] = quantity;
+    setInputList(list);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Billing Form</h1>
+   <label htmlFor="">Customer Name</label> 
+   <input type = "text" placeholder="Customer name here" ></input>
+
+   <hr/>
+   <hr/>
+{ 
+inputList.map( (x,i) => {
+  return(
+   <div>
+   <label htmlFor="">Select Product</label>
+    <select name="selectItem" onChange={e=>handleinputchange(e,i) }>
+      {options.map((option) => (
+        <option value={option.value}>{option.label}</option>
+      ))}
+    </select>
+    <label htmlFor="">Select Product</label>
+    <input type='number' name="selectQuantity" placeholder = "Quantity" onChange={e=>handleinputchange(e,i)}></input>
+    {
+
+    }
+    { inputList.length-1===i &&
+    <button onClick= { handleaddclick}>Add More Items</button>
+}
     </div>
   );
+})}
+    <hr/>
+    <hr/>
+   <button>Create Bill</button>
+   <div className='Table'>
+      <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+      </thead>
+      <tbody>
+        {}
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          
+        </tr>
+      </tbody>
+      </table>
+   </div>
+
+
+    
+    </div>
+  )
 }
 
 export default App;
